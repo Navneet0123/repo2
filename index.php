2b14eb484520b3a321940bf04769647c73325ea3
Navneet <navneetsinghchoudhary19@gmail.com>
2020-08-01 21:51:24 +0530

    Add index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+ Volume in drive E has no label.
+ Volume Serial Number is 4C6E-CF3D
+
+ Directory of E:\cc\DEVOPS\my project
+
+01-Aug-20  09:50 PM    <DIR>          .
+01-Aug-20  09:50 PM    <DIR>          ..
+01-Aug-20  08:15 PM                22 index.html
+01-Aug-20  09:50 PM                 0 index.php
+01-Aug-20  08:07 PM               345 test1.txt
+               3 File(s)            367 bytes
+               2 Dir(s)  230,748,549,120 bytes free
